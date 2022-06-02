18f51eff49ee1bb133c3f0389ca70b50a04313a8
Fábio Morato <fabiolmorato@dcc.ufmg.br>
2022-06-02 11:51:05 -0300

    feat: solve game use case

---
+++ src/usecases/SolveGameUseCase.py
@@ -0,0 +1,26 @@
+from interfaces.ISolver import ISolver
+from interfaces.IGame import IGame
+from typing import Callable, List
+
+class SolveGameUseCase:
+  def __init__(self, game: IGame, solver: ISolver, create_game: Callable[[List[int]], IGame], print_steps: bool = False):
+    self.game = game
+    self.solver = solver
+    self.print_steps = print_steps
+    self.create_game = create_game
+
+  def _print(self):
+    new_game = self.create_game(self.game.initial_state)
+    new_game.print_curr()
+    print()
+    
+    for move in self.game.moves:
+      new_game.move(move)
+      new_game.print_curr()
+      print()
+
+  def execute(self):
+    self.solver.solve()
+    print(len(self.game.moves), end='\n\n')
+    if (self.print_steps):
+      self._print()
