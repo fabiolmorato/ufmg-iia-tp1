6572b0701e50697c604ed55d1779d1744dd96a07
Fábio Morato <fabiolmorato@dcc.ufmg.br>
2022-06-02 16:41:33 -0300

    feat: BFS game solving

---
+++ src/domain/BFS.py
@@ -0,0 +1,29 @@
+from interfaces.IGame import IGame, GameMoves
+from interfaces.ISolver import ISolver
+from typing import List
+from time import sleep
+
+moves = [GameMoves.UP, GameMoves.DOWN, GameMoves.LEFT, GameMoves.RIGHT]
+
+class BFS(ISolver):
+  def __init__(self, game: IGame):
+    self.queue = [game]
+
+  def check_game_solved(self, game):
+    correct_positions = 0
+    for i in range(8):
+      if game.state[i] == i + 1:
+        correct_positions += 1
+    return correct_positions == 8
+  
+  def solve(self):
+    for game in self.queue:
+      sleep(0.5)
+      if self.check_game_solved(game):
+        self.queue = []
+        return game
+      for move in moves:
+        if game.can_move(move):
+          copy = game.copy()
+          copy.move(move)
+          self.queue.append(copy)
