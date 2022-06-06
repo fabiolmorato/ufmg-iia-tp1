0a2b1a7cce1c428c21d46e2a42a8aa2b182e66f1
Fábio Morato <fabiolmorato@dcc.ufmg.br>
2022-06-06 15:46:22 -0300

    chore: testing and running scripts

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python3 src/main.py $1 $(cat toys/$2.txt) $3
---
+++ tests/run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+cd ..;
+
+run-tests() {
+  ALGORITHM=$1;
+  if [[ $ALTERNATE_HEURISTIC = "True" ]]; then
+    ALGORITHM="$1_ALTERNATE";
+  fi
+
+  echo "Running tests for algorithm $ALGORITHM...";
+
+  mkdir -p results/$ALGORITHM;
+  for i in $(seq 0 $2); do
+    echo "└── Running test $i...";
+    python3 src/main.py $1 $(cat toys/$i.txt) PRINT > results/$ALGORITHM/$i;
+  done
+}
+
+run-tests A 24
+ALTERNATE_HEURISTIC=True run-tests A 16
+run-tests B 14
+run-tests G 16
+ALTERNATE_HEURISTIC=True run-tests G 16
+run-tests I 14
+run-tests H 16
+run-tests U 14
